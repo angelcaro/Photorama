e088f00feb0d896d4c3e938fe6f7fb9d8c45d9f0
Angel Caro <angelcaro@angels-macbook-pro.local>
2016-04-27 10:44:23 -0600

    Created PhotosViewController file.

---
+++ Photorama/PhotosViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PhotosViewController.swift
+//  Photorama
+//
+//  Created by Angel Caro on 4/27/16.
+//  Copyright © 2016 AngelCaro. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
